f1abe7b3c06ebcb83d755da1fc98a72d389f4cec
Felipe Moya <fmoyagallo@gmail.com>
2025-08-24 00:59:11 -0400

    Entrega 2 primera version

---
+++ app.js
@@ -0,0 +1,130 @@
+// Los datos se simulan como si fueran traídos de una base de datos, pero por ahora están dentro de una funcion
+const cargarCatalogoDesdeBD = () => [
+  {
+    id: 1,
+    nombre: "Alimento Acana",
+    precio: 22990,
+    categoria: "Comida",
+  },
+  {
+    id: 2,
+    nombre: "Alimento Bravery",
+    precio: 19990,
+    categoria: "Comida",
+  },
+  {
+    id: 3,
+    nombre: "Alimento Leonardo",
+    precio: 24990,
+    categoria: "Comida",
+  },
+  {
+    id: 4,
+    nombre: "Churu 4 unidades",
+    precio: 2900,
+    categoria: "Snacks",
+  },
+  {
+    id: 5,
+    nombre: "Leonardo Latas 200g",
+    precio: 3000,
+    categoria: "Snacks",
+  },
+  {
+    id: 6,
+    nombre: "Catit Nuna insecto",
+    precio: 4490,
+    categoria: "Snacks",
+  },
+  {
+    id: 7,
+    nombre: "Pelota Automática",
+    precio: 5000,
+    categoria: "Juguete",
+  },
+  {
+    id: 8,
+    nombre: "Raton Peludo",
+    precio: 1500,
+    categoria: "Juguete",
+  },
+  {
+    id: 9,
+    nombre: "Catit Treat Puzzle",
+    precio: 20990,
+    categoria: "Juguete",
+  },
+  {
+    id: 10,
+    nombre: "Antiparasitario",
+    precio: 14000,
+    categoria: "Farmacia",
+  },
+];
+
+// El carrito será una clase, cuya llave "items" será un objeto cuyas keys serán los id de los productos presentes en el carro, y los
+// valores asociados a esas claves será la cantidad de ese producto presente en el carro. Esto facilita implementar metodos como
+// Agregar al carro, remover del carro, vaciar carro, actualizar cantidades, etc
+class Carro {
+  // si el carro ya existe, usara el objeto guardado en el localStorage. Si no, inicializará un objeto vacío
+  constructor(items = {}) {
+    this.items = items;
+  }
+
+  agregarAlCarro(id) {
+    this.items[id] = (this.items[id] || 0) + 1;
+  }
+
+  eliminarDelCarro(id) {
+    delete this.items[id];
+  }
+
+  vaciarCarro() {
+    this.items = {};
+  }
+
+  actualizarCantidad(id, cantidad) {
+    if (cantidad <= 0) {
+      this.eliminarDelCarro(id);
+    } else {
+      this.items[id] = cantidad;
+    }
+  }
+
+  obtenerCantidadItems() {
+    let cantidadItems = 0;
+    for (let id in this.items) {
+      cantidadItems += this.items[id];
+    }
+    return cantidadItems;
+  }
+}
+
+// definimos funcion para actualizar la cantidad de elementos en el carro que se ve en el HTML
+const actualizarElementosCarro = (carro) => {
+  cantidadHTML = document.getElementById("cantidad-productos");
+  cantidadHTML.textContent = `(${carro.obtenerCantidadItems()})`;
+};
+
+// definimos una funcion para guardar el carrito en el localStorage
+const guardarEnStorage = (carro) => {
+  localStorage.setItem("carritoCR", JSON.stringify(carro.items));
+  actualizarElementosCarro(carro);
+};
+
+// funcion para inicializar carrito
+const obtenerCarrito = () => {
+  let carritoJSON = localStorage.getItem("carritoCR");
+  let carrito;
+  // si el carrito existe, lo rescata desde el localStorage. Si no (carritoJSON = null), lo va a crear en el localStorage
+  if (carritoJSON) {
+    carrito = new Carro(JSON.parse(carritoJSON));
+    actualizarElementosCarro(carrito);
+  } else {
+    carrito = new Carro();
+    guardarEnStorage(carrito);
+  }
+  return carrito;
+};
+
+obtenerCarrito();
---
+++ carrito.js
@@ -0,0 +1,92 @@
+const catalogo = cargarCatalogoDesdeBD();
+const carrito = obtenerCarrito();
+
+const renderizarCarrito = () => {
+  let filasCarrito = document.getElementById("carrito");
+  let valorTotalHTML = document.getElementById("total");
+
+  let htmlCarrito = "";
+  let valorTotal = 0;
+  if (carrito.obtenerCantidadItems()) {
+    for (let id in carrito.items) {
+      let productoCarro = catalogo.find(
+        (producto) => producto.id === Number(id)
+      );
+
+      let cantidadProducto = carrito.items[id];
+      let precioProducto = productoCarro.precio;
+      let subtotal = cantidadProducto * precioProducto;
+      valorTotal += subtotal;
+
+      htmlCarrito += `
+        <tr>
+            <td>${productoCarro.nombre}</td>
+            <td>
+                <input 
+                type="number" 
+                class="form-control form-control-sm" 
+                value="${cantidadProducto}" 
+                min="1" 
+                onchange="actualizarCantidadCarro(${id}, Number(this.value))">
+            </td>
+            <td>$${productoCarro.precio.toLocaleString("es-CL")}</td>
+            <td>$${subtotal.toLocaleString("es-CL")}</td>
+            <td><button onclick="eliminarProducto(${id})" class="btn btn-sm btn-danger">Eliminar</button></td>
+        </tr>
+      `;
+    }
+  } else {
+    htmlCarrito = `
+        <tr>
+            <td>Carrito Vacío</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+        </tr>`;
+  }
+  valorTotalHTML.textContent = `$${valorTotal.toLocaleString("es-CL")}`;
+  filasCarrito.innerHTML = htmlCarrito;
+};
+
+const eliminarProducto = (id) => {
+  carrito.eliminarDelCarro(id);
+  guardarEnStorage(carrito);
+  renderizarCarrito();
+};
+
+const actualizarCantidadCarro = (id, nuevaCantidad) => {
+  carrito.actualizarCantidad(id, nuevaCantidad);
+  guardarEnStorage(carrito);
+  renderizarCarrito();
+};
+
+const vaciarCarro = () => {
+  carrito.vaciarCarro();
+  guardarEnStorage(carrito);
+  renderizarCarrito();
+};
+
+let botonVaciar = document.getElementById("boton-vaciar");
+botonVaciar.addEventListener("click", vaciarCarro);
+
+let mensajeCompra = document.getElementById("mensaje-compra");
+let botonPagar = document.getElementById("boton-pagar");
+botonPagar.addEventListener("click", () => {
+  if (carrito.obtenerCantidadItems()) {
+    mensajeCompra.innerHTML = `
+        <div class="alert alert-success text-center">
+        ¡Gracias por comprar en <strong>Cat Republic</strong>!
+        </div>
+        `;
+    vaciarCarro();
+  } else {
+    mensajeCompra.innerHTML = `
+        <div class="alert alert-danger text-center">
+        Tu carrito está vacío 🛒
+        </div>
+        `;
+  }
+});
+
+renderizarCarrito();
